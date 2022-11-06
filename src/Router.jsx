24a0234dfaae4489589089733c71500e87752d38
Shreyas <bshreyasbaliga@gmail.com>
2022-11-07 02:53:35 +0530

    Added route level lazy loading

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux'
 
+import Spinner from './components/Spinner';
+
 import { store } from './store'
 
-import UserValidateWrapper from './UserValidateWrapper';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Feeds from './components/Feeds';
-import FeedInfo from './components/FeedInfo';
-import NotFound from './components/NotFound';
+const UserValidateWrapper = React.lazy(() => import('./UserValidateWrapper'));
+const Login = React.lazy(() => import('./components/Login'));
+const Dashboard = React.lazy(() => import('./components/Dashboard'));
+const Feeds = React.lazy(() => import('./components/Feeds'));
+const FeedInfo = React.lazy(() => import('./components/FeedInfo'));
+const NotFound = React.lazy(() => import('./components/NotFound'));
+
+
 
 function MountApp() {
   return (
     <Provider store={store}>
-      <UserValidateWrapper>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Navigate to='/login' />} />
-            <Route path='/login' element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />}>
-              <Route path="" element={<Navigate to='/dashboard/feeds' />} />
-              <Route path='feeds' element={<Feeds />} />
-              <Route path='feeds/:feedId' element={<FeedInfo />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </Router>
-      </UserValidateWrapper>
+      <Suspense fallback={<Spinner />}>
+        <UserValidateWrapper>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Navigate to='/login' />} />
+              <Route path='/login' element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />}>
+                <Route path="" element={<Navigate to='/dashboard/feeds' />} />
+                <Route path='feeds' element={<Feeds />} />
+                <Route path='feeds/:feedId' element={<FeedInfo />} />
+              </Route>
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </Router>
+        </UserValidateWrapper>
+      </Suspense>
     </Provider>
   );
 }
